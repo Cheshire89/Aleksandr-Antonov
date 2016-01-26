61eb6a66400de448903867213a27e79a1eafd019
Aleksandr Antonov <antonovfineart@gmail.com>
2016-01-26 14:05:56 -0700

    Moved set functions to match the form styling

--- php/send.php
+++ php/send.php
@@ -52,23 +52,7 @@ function setMessage($data){
     return $message;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")	{
-	$name = setName($_POST["name"]);
-	$email = setEmail($_POST["email"]);
-	$message = setMessage($_POST["message"]);
-}
 	
-	
-	$to = "antonovfineart@gmail.com";
-    $subject = "From: ".$name." My personal site";
-    
-	$messages = "Name: ".$name."\r\n  E-mail: ".$email."\r\n Message: ".$message;
-    $headers = "From:" . $name;
-
-    if(parametersSet($name,$email,$message)){
-    	mail($to, $subject, $messages, $headers);
-    	echo '<h5 id="set">Your message had been sent<h5>'; 
-    } 
 ?>
 <style type="text/css">
 #set{
@@ -83,26 +67,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")	{
     <form id="hire" method="POST" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	      <div class="field name-box">
 		        <input type="text" id="name" name="name" value="<?php echo $name;?>" placeholder="Greetings!" required />
-        		<label for="name">Name</label>
+        		<label for="name">Name*</label>
 		        <span class="glyphicon-ok"></span>
-		        <span class="error"><?php echo $nameErr; ?></span>
+		        <span class="error">
+		        <?php
+		        	if ($_SERVER["REQUEST_METHOD"] == "POST")	{
+						$name = setName($_POST["name"]);
+					}
+		        ?>
+		        </span>
 	      </div>
 
 	      <div class="field email-box">
 		        <input type="text" type="email" id="email" name="email" value="<?php echo $email;?>" placeholder="Name@email.com" required />
-		        <label for="email">Email</label>
+		        <label for="email">Email*</label>
 		        <span class="glyphicon-ok"></span>
-		        <span class="error"><?php echo $emailErr; ?></span>
+		        <span class="error">
+		        	<?php
+		        	if ($_SERVER["REQUEST_METHOD"] == "POST")	{
+						$email = setEmail($_POST["email"]);
+					}
+		        	?>
+		        </span>
 	      </div>
 
 	      <div class="field msg-box">
 		        <textarea id="msg" rows="4" name="message" value="<?php echo $message;?>" placeholder="How may I help you ?" required /></textarea>
-		        <label for="msg">Msg</label>
+		        <label for="msg">Msg*</label>
 		        <span class="glyphicon-ok"></span>
-		        <span class="error"><?php echo $messageErr; ?></span>
+		        <span class="error">
+		        	<?php
+		        	if ($_SERVER["REQUEST_METHOD"] == "POST")	{
+						$message = setMessage($_POST["message"]);
+					}
+		        	?>
+		        </span>
 	      </div>
 	      <div id="send">
 	      <input id="send_btn" class="button" type="submit" value="Send" />
 	      </div>
   </form>
     </div>
+<?php
+	$to = "antonovfineart@gmail.com";
+    $subject = "From: ".$name." My personal site";
+    
+	$messages = "Name: ".$name."\r\n  E-mail: ".$email."\r\n Message: ".$message;
+    $headers = "From:" . $name;
+
+    if(parametersSet($name,$email,$message)){
+    	mail($to, $subject, $messages, $headers);
+    	echo '<h5 id="set">Your message had been sent<h5>'; 
+    } 
+?>
